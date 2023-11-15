bf542856de108a47c32bc534ee7f9be622526f8b
abrarabedin001 <abrarabedin001@gmail.com>
2023-11-15 20:54:23 +0600

    should work !

--- main/background.ts
+++ main/background.ts
@@ -2,6 +2,14 @@ import path from 'path'
 import { app, ipcMain } from 'electron'
 import serve from 'electron-serve'
 import { createWindow } from './helpers'
+import fs from 'fs'
+import Store from 'electron-store'
+// const db = require('better-sqlite3')('../test.db', { verbose: console.log });
+const sqlite3 = require('sqlite3').verbose();
+let sql;
+const db = new sqlite3.Database('../norSaas.db', (err) => { if (err) return console.error(err.message); });
+
+
 
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -35,6 +43,31 @@ app.on('window-all-closed', () => {
   app.quit()
 })
 
+
+type StoreType = {
+  messages: string[]
+}
+const store = new Store<StoreType>({ name: 'messages' })
+
 ipcMain.on('message', async (event, arg) => {
-  event.reply('message', `${arg} kaka World!`)
+  fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  // db.run("CREATE TABLE lorem (info TEXT)");
+  // const row = db.prepare('SELECT * FROM lorem')
+  const row1 = db.prepare(`INSERT INTO lorem (info) VALUES ('Sample data')`)
+  const row = db.prepare('SELECT * FROM lorem')
+
+  console.log('row', row)
+  // console.log(row.firstName, row.lastName, row.email);
+  // console.log(row)
+  // fs.writeFile('mynewfile3.txt', row, function (err) {
+  //   if (err) throw err;
+  //   console.log('Saved!');
+  // });
+  // store.set('messages2', "testtesttest")
+  let data = store.get('messages2')
+  console.log('arg', arg, row)
+  event.reply('message', `${data} kaka World!`)
 })

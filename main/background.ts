4cdf62fa473baa5759a034bafc6029151f8fc1ee
abrarabedin001 <abrarabedin001@gmail.com>
2023-11-15 21:21:08 +0600

    should work !

--- main/background.ts
+++ main/background.ts
@@ -7,8 +7,8 @@ import Store from 'electron-store'
 // const db = require('better-sqlite3')('../test.db', { verbose: console.log });
 const sqlite3 = require('sqlite3').verbose();
 let sql;
-const db = new sqlite3.Database('../norSaas.db', (err) => { if (err) return console.error(err.message); });
-
+// const db = new sqlite3.Database('../norSaas.db', (err) => { if (err) return console.error(err.message); });
+const db = new sqlite3.Database('sqlite.db');
 
 
 const isProd = process.env.NODE_ENV === 'production'
@@ -54,20 +54,33 @@ ipcMain.on('message', async (event, arg) => {
     if (err) throw err;
     console.log('Saved!');
   });
-  // db.run("CREATE TABLE lorem (info TEXT)");
-  // const row = db.prepare('SELECT * FROM lorem')
-  const row1 = db.prepare(`INSERT INTO lorem (info) VALUES ('Sample data')`)
-  const row = db.prepare('SELECT * FROM lorem')
+  db.serialize(() => {
+    // try { db.run("CREATE TABLE lorem (info TEXT)"); } catch {
+    //   console.log("table already exist")
+    // }
 
-  console.log('row', row)
-  // console.log(row.firstName, row.lastName, row.email);
-  // console.log(row)
-  // fs.writeFile('mynewfile3.txt', row, function (err) {
-  //   if (err) throw err;
-  //   console.log('Saved!');
-  // });
-  // store.set('messages2', "testtesttest")
+
+    const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (let i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, info FROM test", (err, row) => {
+      console.log(row.id + ": " + row.info + "11");
+    });
+  });
+
+  db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+    console.log(row.id + ": " + row.info + "11");
+  })
+
+
+
+
+
+  store.set('messages2', "testtesttest")
   let data = store.get('messages2')
-  console.log('arg', arg, row)
+  console.log('arg', arg)
   event.reply('message', `${data} kaka World!`)
 })

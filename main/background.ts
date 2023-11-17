897c3c6898522088b5d7b09d747b0a8c3c57b499
abrarabedin001 <abrarabedin001@gmail.com>
2023-11-17 14:49:43 +0600

    trying to retrive the image

--- main/background.ts
+++ main/background.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { app, ipcMain } from 'electron'
+import { app, ipcMain, ipcRenderer } from 'electron'
 import serve from 'electron-serve'
 import { createWindow } from './helpers'
 import fs from 'fs'
@@ -50,18 +50,75 @@ type StoreType = {
 const store = new Store<StoreType>({ name: 'messages' })
 
 ipcMain.on('message', async (event, arg) => {
+  // fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  //   if (err) throw err;
+  //   console.log('Saved!');
+  // });
+  // let imageData = fs.readFileSync(arg);
+
+  // let destinationPath = `D://Javascript/test-app/images/test2`;
+
+  // if (!fs.existsSync(destinationPath)) {
+  //   fs.mkdirSync(destinationPath, { recursive: true });
+  // }
+
+  // let fileName = Date.now()
+
+  // fs.writeFile(`${destinationPath}/${fileName}.jpg`, imageData, (err) => {
+  //   if (err) throw err;
+
+  //   console.log('The file has been saved!');
+  // });
+  console.log('arg', typeof arg)
+  event.reply('message', 'World!')
+})
+
+ipcMain.on('save', async (event, arg) => {
   fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
   let imageData = fs.readFileSync(arg);
-  // console.
-  let destinationPath = `D://Javascript/test-app/images/2.jpg`;
-  // fs.writeFileSync(destinationPath, imageData);
-  fs.writeFile(destinationPath, imageData, (err) => {
+
+  let destinationPath = `D://Javascript/test-app/images/test2`;
+
+  if (!fs.existsSync(destinationPath)) {
+    fs.mkdirSync(destinationPath, { recursive: true });
+  }
+
+  let fileName = Date.now()
+
+  fs.writeFile(`${destinationPath}/${fileName}.jpg`, imageData, (err) => {
     if (err) throw err;
+
     console.log('The file has been saved!');
   });
   console.log('arg', imageData)
   event.reply('message', `kaka World!`)
 })
+
+
+ipcMain.on('retrieve', async (event, arg) => {
+  const readImageAsBase64 = (filePath) => {
+    const imageData = fs.readFileSync('/images/test2/1700065754526.jpg');
+    return imageData.toString('base64');
+  };
+
+  const base64Image = readImageAsBase64(arg);
+  // let imageData = fs.readFileSync(arg);
+  // ... rest of your code to save the image ...
+
+  // Send the binary data to the front-end
+
+  console.log('arg', base64Image)
+
+  event.reply('retrieve', base64Image);
+
+
+})
+
+// ipcRenderer.on('imageData', (event, imageData) => {
+//   // Convert the Buffer to a Blob
+//   let blob = new Blob([imageData], { type: 'image/jpeg' });
+//   // Now, 'blob' is a Blob object representing the image
+// });
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
+import { ipcRenderer } from 'electron'
 
 export default function HomePage() {
   const [message, setMessage] = React.useState('No message found')
   const [file, setFile] = React.useState<File | null>(null)
+  const [imageSrc, setImageSrc] = useState('');
 
   React.useEffect(() => {
+    window.ipc.on('retrieve', (message: string) => {
+      // const base64String = btoa(String.fromCharCode(...new Uint8Array(message)));
+      setMessage(message)
+    })
     window.ipc.on('message', (message: string) => {
       setMessage(message)
     })
   }, [])
+
+  // const = byteString = message
+
+  useEffect(() => {
+    const binaryString = hexToBinary(message); // Convert hex string to binary
+    const base64String = binaryToBase64(binaryString); // Convert binary to Base64
+    setImageSrc(`data:image/jpeg;base64,${base64String}`);
+  }, [message]);
+
+  const hexToBinary = (hexString) => {
+    const bytes = new Uint8Array(Math.ceil(hexString.length / 2));
+    for (let i = 0, j = 0; i < hexString.length; i += 2, j++) {
+      bytes[j] = parseInt(hexString.substr(i, 2), 16);
+    }
+    return bytes;
+  };
+  const binaryToBase64 = (binary) => {
+    const blob = new Blob([binary], { type: 'image/jpeg' });
+    return URL.createObjectURL(blob);
+  };
+
+  
+
+  
 
   return (
     <React.Fragment>
@@ -36,13 +66,24 @@ export default function HomePage() {
       <div>
         <button
           onClick={() => {
-            console.log(file.path)
-            window.ipc.send('message', file.path)
+         
+            window.ipc.send('save', file.path)
           }}
         >
           Test IPC
+         
         </button>
+        <button onClick={()=>window.ipc.send('retrieve', `D://Javascript/test-app/images/test2/1700065754526.jpg`)}>
+          test render
+        </button>
+
+
+        <img src={`data:image/jpeg;base64,${imageSrc}`} alt="From Server" />;
+        <img src={`../../images/test2/1700065830898.jpg`} alt="From Server" />;
         <p>{message}</p>
+        <p>{imageSrc}</p>
+
+
       </div>
     </React.Fragment>
   )
